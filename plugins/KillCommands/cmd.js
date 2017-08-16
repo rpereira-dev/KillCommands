29d7beef96d2ec5f0854687feb0121a54ebe4b0d
rpereira <cedquen@hotmail.fr>
2017-08-16 18:16:15 +0200

    cmd

--- plugins/KillCommands/cmd.js
+++ plugins/KillCommands/cmd.js
@@ -9,13 +9,15 @@ Global variables:
 */
 
 //GIVE SCRIPT
-/*
-if (killerIsPlayer) {
-	"give " + killedName + " 1 2"
-}
-*/
 
+if (killerIsPlayer && worldName == "spawn") {
+	MAX_AMOUNT = 10;
+	MIN_AMOUNT = 1;
+	ITEM_ID = "log";
+	amount = Math.floor(Math.random() * MAX_AMOUNT) + MIN_AMOUNT;
+	"give " + killerName + " " + ITEM_ID + " " + amount;
+}
 
 //DEBUG SCRIPT:
 
-"say " + "[KilledCommands] " + killedName + " was killed by " + killerName + "(killerIsPlayer=" + killerIsPlayer + ") in world: " + worldName
+//"say " + "[KilledCommands] " + killedName + " was killed by " + killerName + "(killerIsPlayer=" + killerIsPlayer + ") in world: " + worldName
